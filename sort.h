80c22f4d699f5f27b3f649be38ba4ba5c73d1083
illusies <john_illusies@yahoo.com>
2022-10-11 18:50:41 -0700

    Functions added

--- sort.h
+++ sort.h
@@ -29,6 +29,6 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 int find_maxnum(int *array, size_t size);
-void fill_array(int *a, int *b, size_t size);
+void fill_array(int *array_a, int *array_b, size_t size);
 
 #endif
