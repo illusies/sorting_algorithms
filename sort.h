c2ef1233eb9af38dda9531ab2040c00f6ebe08a3
illusies <john_illusies@yahoo.com>
2022-10-12 18:07:11 -0700

    Additional functions added

--- sort.h
+++ sort.h
@@ -19,16 +19,44 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+
+/*Bubble sort function*/
 void bubble_sort(int *array, size_t size);
+
+/*Insertion sort function*/
 void insertion_sort_list(listint_t **list);
+
+/*Selection sort function*/
 void selection_sort(int *array, size_t size);
+
+/*Quick sort function*/
 void quick_sort(int *array, size_t size);
 void swap(int *arr, int low, int high, size_t size);
 int partition(int *arr, int low, int high, size_t size);
+
+/*Shell sort function*/
 void shell_sort(int *array, size_t size);
+
+/*Cocktail sort function*/
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 void cocktail_sort_list(listint_t **list);
+
+/*Counting sort function*/
 void counting_sort(int *array, size_t size);
 int find_maxnum(int *array, size_t size);
 void fill_array(int *array_a, int *array_b, size_t size);
+
+/*Merge sort function*/
+void merge_sort(int *array, size_t size);
+void topdownmergesort(int *array_a, int *array_b, int size);
+void topdownsplitmerge(int *array_a, int *array_b, size_t size, size_t left, size_t right);
+void topdownmerge(int *array_a, int *array_b, size_t size, size_t left, size_t mid, size_t right);
+void copyarray(int *array_a, int ibegin, int iend, int *array_b);
+
+/*Heap sort function*/
+void swap_ints(int *a, int *b);
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
 
 #endif
