60740e6577ee7008b4e4a11bc436537f5e0057d9
John McDove <96707236+illusies@users.noreply.github.com>
2022-10-12 20:34:07 -0500

    Bitonic sort function

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,82 @@
+#include "sort.h"
+
+/**
+ * swap_ints - function that swaps two integers in an array
+ * @a: swap integer 1
+ * @b: swap integer 2
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bitonic_merge - function that sorts a bitonic sequence inside an array of integers
+ * @array: array of integers
+ * @size: size of the array
+ * @start: starting index of array sequence to be sorted
+ * @seq: sequence sort size
+ * @flow: what direction to sort
+ */
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow)
+{
+	size_t i, jump = seq / 2;
+
+	if (seq > 1)
+	{
+		for (i = start; i < start + jump; i++)
+		{
+			if ((flow == UP && array[i] > array[i + jump]) ||
+			    (flow == DOWN && array[i] < array[i + jump]))
+				swap_ints(array + i, array + i + jump);
+		}
+		bitonic_merge(array, size, start, jump, flow);
+		bitonic_merge(array, size, start + jump, jump, flow);
+	}
+}
+
+/**
+ * bitonic_seq - function that converts an array of integers into a bitonic sequence
+ * @array: array of integers
+ * @size: size of the array
+ * @start: The starting index of a block of the building bitonic sequence.
+ * @seq: The size of a block of the building bitonic sequence.
+ * @flow: The direction to sort the bitonic sequence block in.
+ */
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
+{
+	size_t cut = seq / 2;
+	char *str = (flow == UP) ? "UP" : "DOWN";
+
+	if (seq > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+
+		bitonic_seq(array, size, start, cut, UP);
+		bitonic_seq(array, size, start + cut, cut, DOWN);
+		bitonic_merge(array, size, start, seq, flow);
+
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+	}
+}
+
+/**
+ * bitonic_sort - function that sorts an array of integers in 
+ * ascending order using the Bitonic sort algorithm
+ * @array: array of integers
+ * @size: size of the array
+ * Return: void
+ */
+void bitonic_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	bitonic_seq(array, size, 0, size, UP);
+}
